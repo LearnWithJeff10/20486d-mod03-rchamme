c7994c3d601e882dddc760d04fd8b0b8dc6631b6
Rita Hammett <hammett.rc@gmail.com>
2020-06-09 22:25:20 -0400

    Testing a change

--- Labfiles/01_PollBall_begin/PollBall/PollBall/Startup.cs
+++ Labfiles/01_PollBall_begin/PollBall/PollBall/Startup.cs
@@ -13,6 +13,7 @@ namespace PollBall
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // Testing a change
         public void ConfigureServices(IServiceCollection services)
         {
         }
